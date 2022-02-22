f42e8dff3b581d23a10bea10fdf88f9f9e700846
Kasper Nikolajsen Dahl <kasp103y@stud.kea.dk>
2022-02-22 15:37:38 +0100

    burger time

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const burgerContent = document.querySelector("nav .burger-content");
 //window.addEventListener("scroll", showHeader);
 //window.addEventListener("scroll", showHeaderFooter);
 burger.addEventListener("click", showBurger);
+burgerContent.addEventListener("click", fjernBurger);
+
+function fjernBurger() {
+  burger.classList.toggle("left-0");
+  burgerContent.classList.toggle("left-0");
+  burger.classList.toggle("left-full");
+  burgerContent.classList.toggle("left-full");
+}
 
 function showBurger() {
   console.log("Hej burger");
